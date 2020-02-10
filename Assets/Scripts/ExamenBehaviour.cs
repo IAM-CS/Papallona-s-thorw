bea12d1842dfb8432ce35a96afff05977f6c10e6
Sceniquette <60893378+Sceniquette@users.noreply.github.com>
2020-02-10 19:30:41 +0100

    Create ExamenBehaviour.cs
    Pues ya estaría uwu

---
+++ Assets/Scripts/ExamenBehaviour.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExamenBehaviour : MonoBehaviour
+{
+
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
